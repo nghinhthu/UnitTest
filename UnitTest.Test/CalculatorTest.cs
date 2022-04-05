6d0c592e6fe26903deeb20f8a0f9cc39d8bba8f9
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2022-04-05 15:57:56 +0700

    Add project files.

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTest.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Add_Test()
+        {
+            Calculator cal = new Calculator();
+            int add_Result = cal.Add(3, 5);
+            Assert.That(add_Result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Sub_Test()
+        {
+            Calculator cal = new Calculator();
+            int sub_Result = cal.Sub(3, 5);
+            Assert.That(sub_Result, Is.EqualTo(8));
+        }
+    }
+}
